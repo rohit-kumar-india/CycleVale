ace1277d01fca85aa34876f675c8ee2dac3ee340
Tanveer <118190047+TanveerAI@users.noreply.github.com>
2024-04-06 15:05:51 +0530

    product Card added

--- frontend/src/Components/Homepage/FeaturedProducts.js
+++ frontend/src/Components/Homepage/FeaturedProducts.js
@@ -1,10 +1,24 @@
+import ProductCard from "../ProductCard";
+import Wrapper from "../Wrapper";
 // components/homepage/FeaturedProducts.js
 const FeaturedProducts = () => (
-    <div>
-      <h2>Featured Products</h2>
-      {/* Map through your products and display them */}
+    <>
+    <Wrapper>
+    <div className="text-center max-w-[800px] mx-auto my-[20px] md:my-[40px]">
+    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+      Explore the World with Our Bicycles
     </div>
+    <div className="text-md md:text-xl">
+    Enjoy your all terrain rides from Hilly mountain rides to off roads thrills with CycleVale Mountain bikes. These bicycles are loaded with world class components like shimano gear, Spur Suspension. With a vast collection of MTB cycles to choose from, CycleVale offers something for every level of rider, from beginners to professionals.
+    </div>
+    </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-20 my-10 px-5  md:px-0">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        </div>
+        </Wrapper>
+      </>
   );
-  
-  export default FeaturedProducts;
-  
+
+export default FeaturedProducts;
--- frontend/src/Components/ProductCard.js
+++ frontend/src/Components/ProductCard.js
@@ -1,13 +1,59 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 
 const ProductCard = ({product}) => {
   return (
-    <div className="border p-4">
-      <h2 className="text-lg font-bold">{product.name}</h2>
-      <p>${product.price}</p>
-      {/* Add to Cart Button or Link to Product Detail */}
+    <>
+  <Link href="/product/1" className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+    <img src="/cyclefront2.jpg" alt="Product image" className="w-full px-5" />
+    <div className="p-4 text-black/[0.9]">
+      <h2>Product Name</h2>
+      <div className="flex items-center text-black/[0.7]">
+      <p className="mr-2 text-lg font-semibold">
+                        $79.00
+        </p>
+      <p className="text-base  font-medium line-through"> $100.00 </p>
+      <p className="ml-auto text-base font-medium text-green-500"> 21% off</p>
+      </div>
     </div>
+  </Link>
+  <Link href="/product/1" className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+    <img src="/cyclefront3.jpg" alt="Product image" className="w-full px-5" />
+    <div className="p-4 text-black/[0.9]">
+      <h2>Product Name</h2>
+      <div className="flex items-center text-black/[0.7]">
+      <p className="mr-2 text-lg font-semibold">
+                        $79.00
+        </p>
+      <p className="text-base  font-medium line-through"> $100.00 </p>
+      <p className="ml-auto text-base font-medium text-green-500"> 21% off</p>
+      </div>
+    </div>
+  </Link>
+
+  <Link href="/product/1" className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+    <img src="/cyclefront4.jpg" alt="Product image" className="w-full px-5" />
+    <div className="p-4 text-black/[0.9]">
+      <h2>Product Name</h2>
+      <div className="flex items-center text-black/[0.7]">
+      <p className="mr-2 text-lg font-semibold">
+                        $79.00
+        </p>
+      <p className="text-base  font-medium line-through"> $100.00 </p>
+      <p className="ml-auto text-base font-medium text-green-500"> 21% off</p>
+      </div>
+    </div>
+  </Link>
+  </>
+
+    // <div className="border p-4">
+    //   <h2 className="text-lg font-bold">{product.name}</h2>
+    //   <p>${product.price}</p>
+    //   {/* Add to Cart Button or Link to Product Detail */}
+    // </div>
   )
 }
 
-export default ProductCard
+export default ProductCard 
