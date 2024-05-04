9b063b6fe1588532591796c3667060eb09e57e2c
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-05-04 17:20:20 +0530

    Added loader in product page

--- frontend/src/pages/products/index.js
+++ frontend/src/pages/products/index.js
@@ -4,22 +4,12 @@ import axios from 'axios';
 import ProductCard from '@/Components/ProductCard';
 import { toast } from 'react-toastify';
 
-// Placeholder data fetching function
-async function fetchProducts() {
-  // Replace with actual data fetching logic
-  try {
-    const response = await axios.get('http://localhost:5000/api/products');
-    return response.data.products;
-  } catch (error) {
-    console.error('Error fetching products:', error);
-  }
-}
-
 const Products = () => {
   const [userId, setUserId] = useState(null);
   const [products, setProducts] = useState([]);
   const [wishlistItems, setWishlistItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [processing, setProcessing] = useState(false); // State to manage processing status
+  const [dynamicText, setDynamicText] = useState('');
 
   const toastOptions = {
     position: "top-right",
@@ -32,10 +22,26 @@ const Products = () => {
     theme: "light",
   }
 
+  // Function for fetching all product
+  const fetchProducts = async () => {
+    try {
+      setDynamicText('Fetching Products...');
+      setProcessing(true);
+      const response = await axios.get('http://localhost:5000/api/products');
+      return response.data.products;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    } finally {
+      setProcessing(false);
+    }
+  }
+
   const fetchWishlistDetails = async (id) => {
-    setIsLoading(true); // Assuming you have an isLoading state to manage UI loading feedback
+    //setIsLoading(true); // Assuming you have an isLoading state to manage UI loading feedback
 
     try {
+      setDynamicText('Fetching Wishlist Details...');
+      setProcessing(true);
       // Fetch the user's wishlist
       const wishlistResponse = await axios.get(`http://localhost:5000/api/users/${id}/wishlists`);
       const wishlists = wishlistResponse.data;
@@ -53,7 +59,7 @@ const Products = () => {
     } catch (error) {
       console.error('Failed to fetch wishlist or product details', error);
     } finally {
-      setIsLoading(false); // Update loading state
+      setProcessing(false);
     }
   };
 
@@ -108,11 +114,6 @@ const Products = () => {
       outOfStock: false
     }
   });
-
-  if (isLoading) {
-    return <div className='mt-[60px] height-[600px]'>Loading Products...</div>;
-  }
-
 
   const handleCheckboxChange = (category, value) => {
     // If category is an object (e.g., duration), handle its nested state
@@ -216,55 +217,26 @@ const Products = () => {
 
             </div>
 
-
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {products.map((product) => {
-                {//wishlistItems.includes(product.id)
-                  //product.id ? 'a':'b'
-                  //isWishlisted({product.name})
-                }
-
                 // Check if the current product ID is in the wishlistItems array
                 const isWishlisted = wishlistItems.includes(product._id);
 
-                // Pass the wishlisted status as a prop to the ProductCard
                 return (
-                  <>
-                    <ProductCard key={product._id} Product={{ ...product, wishlisted: isWishlisted }} />
-                    {/* <Link href={`/products/${product._id}`} className="transform overflow-hidden hover:shadow-lg bg-white duration-200 hover:scale-105 cursor-pointer">
-                    <img src="/images/cyclefront2.jpeg" alt="Product image" className="w-full px-5" />
-                    <div className="p-4 text-black/[0.9]">
-                      <h2 className="font-bold text-xl mb-1" >{product.name}</h2>
-                      <div className="flex items-center mb-1">
-                        <span className="ml-1 text-sm">{product.rating}</span>
-                        <svg className="w-5 h-5 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.921-.755 1.688-1.54 1.118l-3.976-2.888a1 1 0 00-1.175 0l-3.976 2.888c-.785.57-1.84-.197-1.54-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.783-.57-.38-1.81.588-1.81h4.915a1 1 0 00.95-.69l1.519-4.674z" />
-                        </svg>
-                        <span className="ml-1 text-sm"> ({product.numReviews} Reviews)</span>
-                      </div>
-                      <div className="flex items-center text-black/[0.7]">
-                        {product.discountPercentage > 0 && currentDate >= new Date(product.discountStart) && currentDate <= new Date(product.discountEnd) ? (
-                          <>
-                            <p className="mr-2 text-lg font-semibold">₹{(product.price - (product.price * product.discountPercentage / 100)).toFixed(2)}</p>
-                            <p className="text-base  font-medium line-through"> ₹{product.price} </p>
-                            <p className="ml-2 text-base font-medium text-green-500"> {product.discountPercentage}% off</p>
-                          </>
-
-                        ) : (
-                          <p className="mr-2 text-lg font-semibold">₹{product.price}</p>
-                        )}
-                      </div>
-                    </div>
-                  </Link> */}
-                  </>
+                  // Passed the wishlisted status as a prop to the ProductCard
+                  <ProductCard key={product._id} Product={{ ...product, wishlisted: isWishlisted }} />
                 );
               })}
             </div>
+            {/* Processing popup */}
+            {processing && (
+              <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+                <div className="text-white text-lg ml-4">{dynamicText}</div>
+              </div>
+            )}
 
           </div>
-
-
-
         </div>
       </div>
     </>
