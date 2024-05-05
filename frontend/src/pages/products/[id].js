523d649028011d734a74058237a4f8fc8e7ec09d
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-05-05 13:44:45 +0530

    made product details page responsive

--- frontend/src/pages/products/[id].js
+++ frontend/src/pages/products/[id].js
@@ -169,12 +169,12 @@ const ProductDetails = () => {
       <Head>
         <title>{product.name}</title>
       </Head>
-      <div className="mt-[60px] w-full flex justify-center flex-row bg-white shadow  mx-auto">
+      <div className="mt-[60px] flex justify-center max-w-7xl  bg-white shadow  mx-auto">
         
-        <div className='max-w-7xl flex' >
+        <div className='flex flex-col md:flex-row'>
           {/* Left Section for Images */}
-            <div className="flex flex-col bg-white px-4 py-5 sm:px-6 w-[50%] h-screen sticky pb-20 top-[60px]" >
-              <div className="absolute right-14 top-6 z-10">
+            <div className="flex flex-col bg-white px-4 py-5 sm:px-6 w-fit h-screen md:sticky pb-20 top-[60px]" >
+              <div className="absolute right-14 md:top-6 top-20 z-10">
                 <button onClick={handleWishlistToggle} className="p-2 bg-white rounded-full shadow-md hover:bg-gray-200 transition duration-250 ease-in-out">
                   {isWishlisted ? (
                     <HeartSolid className="h-6 w-6 text-red-500" />
@@ -218,50 +218,50 @@ const ProductDetails = () => {
         </div>
 
         {/* Right Section for product details */}
-        <div className="px-4 sm:px-6 border-t border-gray-200 flex-grow">
+        <div className="px-4 sm:px-6 border-t border-gray-200">
           
-        <div className="py-4">
-          <h2 className="font-bold text-xl leading-6 text-gray-500 my-2">
-            {product.brand}
-          </h2>
-          <h3 className="font-bold text-lg leading-6 text-gray-900 max-w-[500px]">
-            {product.name}
-          </h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">
-            Product details and specifications.
-          </p>
-        </div>
-        <div className="flex items-center mb-2">
-          <span className="text-m">{product.rating}</span>
-          <svg className="w-5 h-5 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.921-.755 1.688-1.54 1.118l-3.976-2.888a1 1 0 00-1.175 0l-3.976 2.888c-.785.57-1.84-.197-1.54-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.783-.57-.38-1.81.588-1.81h4.915a1 1 0 00.95-.69l1.519-4.674z" />
-          </svg>
-          <span className="ml-2 text-sm"> ({product.numReviews} Reviews)</span>
-        </div>
-        <div className=''>
-         <div className='font-semibold text-green-600'>Special Price</div>
-         {isDiscountActive ? (
-                  <>
-                  
-                    <span className="font-bold text-xl text-green-600">₹{discountedPrice}</span>
-                    <span className="ml-2 line-through">₹{product.price}</span>{" "}
-                    <span className="font-bold ml-2 text-xl text-green-600">{product.discountPercentage}% off</span>
-                  </>
-                ) : (
-                  <span className="font-bold text-xl">₹{product.price}</span>
-                )}
-        </div>
-        <div className="w-full bg-white py-5">
-              <dt className="text-xl font-medium text-gray-900 py-2">
-              Product Description
-              </dt>
+          <div className="py-4">
+            <h2 className="font-bold text-xl leading-6 text-gray-500 my-2">
+              {product.brand}
+            </h2>
+            <h3 className="font-bold text-lg leading-6 text-gray-900 max-w-[500px]">
+              {product.name}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Product details and specifications.
+            </p>
+          </div>
+          <div className="flex items-center mb-2">
+            <span className="text-m">{product.rating}</span>
+            <svg className="w-5 h-5 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.921-.755 1.688-1.54 1.118l-3.976-2.888a1 1 0 00-1.175 0l-3.976 2.888c-.785.57-1.84-.197-1.54-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.783-.57-.38-1.81.588-1.81h4.915a1 1 0 00.95-.69l1.519-4.674z" />
+            </svg>
+            <span className="ml-2 text-sm"> ({product.numReviews} Reviews)</span>
+          </div>
+          <div className=''>
+            <div className='font-semibold text-green-600'>Special Price</div>
+            {isDiscountActive ? (
+                      <>
+                      
+                        <span className="font-bold text-xl text-green-600">₹{discountedPrice}</span>
+                        <span className="ml-2 line-through">₹{product.price}</span>{" "}
+                        <span className="font-bold ml-2 text-xl text-green-600">{product.discountPercentage}% off</span>
+                      </>
+                    ) : (
+                      <span className="font-bold text-xl">₹{product.price}</span>
+                    )}
+          </div>
+          <div className="w-full bg-white py-5">
+            <dt className="text-xl font-medium text-gray-900 py-2">
+            Product Description
+            </dt>
 
-              <dd className="mt-1 text-sm text-gray-900 pl-4 sm:col-span-2 max-w-prose">
-                {product.description}
-              </dd>
+            <dd className="mt-1 text-sm text-gray-900 pl-4 sm:col-span-2 max-w-prose">
+              {product.description}
+            </dd>
 
-            </div>
-            <ReviewList reviews={product.reviews}/>
+          </div>
+          <ReviewList reviews={product.reviews}/>
           <Reviews/>
         </div>
       </div>
