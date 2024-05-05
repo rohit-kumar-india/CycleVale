02ac55879f26a97da6434e095a9f303516c83dd8
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-05-05 13:22:21 +0530

    Added Brand name in product description page

--- frontend/src/pages/products/[id].js
+++ frontend/src/pages/products/[id].js
@@ -220,15 +220,18 @@ const ProductDetails = () => {
         {/* Right Section for product details */}
         <div className="px-4 sm:px-6 border-t border-gray-200 flex-grow">
           
-        <div className="py-5">
-          <h3 className="font-bold text-xl leading-6 text-gray-900 max-w-prose">
+        <div className="py-4">
+          <h2 className="font-bold text-xl leading-6 text-gray-500 my-2">
+            {product.brand}
+          </h2>
+          <h3 className="font-bold text-lg leading-6 text-gray-900 max-w-[500px]">
             {product.name}
           </h3>
           <p className="mt-1 max-w-2xl text-sm text-gray-500">
             Product details and specifications.
           </p>
         </div>
-        <div className="flex items-center mb-1">
+        <div className="flex items-center mb-2">
           <span className="text-m">{product.rating}</span>
           <svg className="w-5 h-5 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.921-.755 1.688-1.54 1.118l-3.976-2.888a1 1 0 00-1.175 0l-3.976 2.888c-.785.57-1.84-.197-1.54-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.783-.57-.38-1.81.588-1.81h4.915a1 1 0 00.95-.69l1.519-4.674z" />
