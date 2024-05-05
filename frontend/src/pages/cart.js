385022c6efdffd5d2f42b0fd82b9115ee90af49c
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-05-05 12:24:30 +0530

    Corrected css and removed unwanted line from cart

--- frontend/src/pages/cart.js
+++ frontend/src/pages/cart.js
@@ -13,7 +13,6 @@ const cart = () => {
   const [cartItems, setCartItems] = useState([]);
   const [processing, setProcessing] = useState(false); // State to manage processing status
   const [dynamicText, setDynamicText] = useState('');
-  const [orderSummary, setOrderSummary] = useState({});
   const [id, setId] = useState('');
   const router = useRouter();
 
@@ -27,14 +26,7 @@ const cart = () => {
     progress: undefined,
     theme: "light",
   }
-  // {
-  //   id: "1",
-  //   name: "",
-  //   image: "/images/cyclefront2.jpeg",
-  //   brand: "",
-  //   quantity: 0,
-  //   price: 0
-  // }
+
   const fetchCartDetails = async (id) => {
     try {
       setDynamicText('Fetching Cart Items...');
@@ -63,11 +55,12 @@ const cart = () => {
       setDynamicText('Removing Item...');
       setProcessing(true);
       const response = await axios.delete(`http://localhost:5000/api/carts/${id}/${productId}`);
-      console.log(response);
-      const updatedCartItems = cartItems.filter(item => item.product !== productId);
-      setCartItems(updatedCartItems);
-      toast.success("Product removed from Cart", toastOptions)
-      //fetchCartDetails(id);
+
+      if (response.status === 200) {
+        const updatedCartItems = cartItems.filter(item => item.product !== productId);
+        setCartItems(updatedCartItems);
+        toast.success("Product removed from Cart", toastOptions)
+      }
     } catch (error) {
       console.error('Failed to delete item from cart', error);
     } finally {
@@ -86,7 +79,7 @@ const cart = () => {
       setDynamicText('Updating Item Quantity...');
       setProcessing(true);
       await axios.patch(`http://localhost:5000/api/carts/${id}/${productId}`, { quantity: newQuantity });
-      console.log("Quantity updated");
+
       // Update quantity in the state/UI
       const updatedCartItems = cartItems.map(item => {
         if (item.product === productId) {
@@ -96,6 +89,7 @@ const cart = () => {
       });
       setCartItems(updatedCartItems);
       toast.success(`Quantity is updated to ${newQuantity}`, toastOptions)
+
     } catch (error) {
       console.error("Failed to update quantity", error);
     } finally {
@@ -103,9 +97,6 @@ const cart = () => {
     }
   };
 
-
-  // console.log("hello")
-  // fetchCartItems('65db29ba433a6266a8d13f40').then(setCartItems);
   useEffect(() => {
     const token = localStorage.getItem('userToken');
     if (!token) {
@@ -114,16 +105,11 @@ const cart = () => {
       let id = localStorage.getItem('userId');
       fetchCartDetails(id);
     }
-    //fetchProductDetails('66123921333320dfc5c3c8e0').then(setCartItems);
-    //fetchCartItems('65db29ba433a6266a8d13f40').then(setCartItems);
-    //}
   }, []);
 
   const totalPrice = cartItems.reduce((total, item) => total + item.productDetails.price * item.quantity, 0);
   const currentDate = new Date();
-  //const isDiscountActive = product.discountPercentage > 0 && currentDate >= new Date(product.discountStart) && currentDate <= new Date(product.discountEnd);
   const discountPrice = cartItems.reduce((totalD, item) =>
-    //(item.productDetails.discountPercentage > 0 && currentDate >= new Date(item.productDetails.discountStart) && currentDate <= new Date(item.productDetails.discountEnd)) ? 
     totalD + ((item.productDetails.discountPercentage > 0 && currentDate >= new Date(item.productDetails.discountStart) && currentDate <= new Date(item.productDetails.discountEnd)) ? item.productDetails.price * item.productDetails.discountPercentage / 100 * item.quantity : 0), 0);
 
   //setOrderSummary({totalPrice,discountPrice, itemNo: cartItems.length})
@@ -155,9 +141,9 @@ const cart = () => {
           {/* Product List */}
           <div className="flex mt-10 mb-5">
             <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
-            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
-            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Quantity</h3>
+            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Price</h3>
+            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Total</h3>
           </div>
           {/* Map through cart items here */}
           {cartItems.map((item) => (
@@ -175,7 +161,6 @@ const cart = () => {
               </div>
               {/* Quantity */}
               <div className="flex justify-center w-1/5">
-                {/* Adjust Quantity Component Here */}
                 <button
                   className="text-xl font-semibold hover:bg-gray-200 border border-slate-300 rounded-full px-2.5"
                   onClick={() => updateQuantity(item.product, item.quantity - 1)}>-</button>
@@ -187,7 +172,6 @@ const cart = () => {
                 <button
                   className="text-xl font-semibold hover:bg-gray-200 border border-slate-300 rounded-full px-2"
                   onClick={() => updateQuantity(item.product, item.quantity + 1)}>+</button>
-                {/* <input className="mx-2 border text-center w-8" type="text" value={item.quantity} readOnly /> */}
               </div>
               {/* Price */}
               <span className="text-center w-1/5 font-semibold text-sm">₹{item.productDetails.price}</span>
@@ -199,7 +183,6 @@ const cart = () => {
         </div>
 
         {/* Right Panel - Pricing Details */}
-
         <OrderSummary TotalPrice={totalPrice} DiscountPrice={discountPrice} itemNo={cartItems.length} />
       </div>
       {/* Processing popup */}
@@ -211,43 +194,6 @@ const cart = () => {
       )}
     </div>
   );
-  // <div id="summary" className='w-full md:w-1/4 px-8 py-10 transition-all duration-1000'>
-  //         <div>
-  //           {/* <div id="summary" className={`${height<250 ? "fixed transition-all duration-1000" : "relative transition-all duration-1000"} transition-all duration-1000`}> */}
-  //           <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
-  //           <div className="flex justify-between mt-10 mb-5">
-  //             <span className="font-semibold text-sm">MRP ({cartItems.length} Items )</span>
-  //             <span className="font-semibold text-sm">₹{totalPrice.toFixed(2)}</span>
-  //           </div>
-  //           <div>
-  //             <label className="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
-  //             <select className="block p-2 text-gray-600 w-full text-sm">
-  //               <option>Standard shipping - ₹10.00</option>
-  //               {/* Additional shipping options here */}
-  //             </select>
-  //           </div>
-  //           <div className="flex justify-between mt-10 mb-5">
-  //             <span className="font-semibold text-sm">Discounts</span>
-  //             <span className="font-semibold text-sm text-green-500">-₹{discountPrice.toFixed(2)}</span>
-  //           </div>
-  //           {/* More pricing details like tax, discounts, etc., can be added here */}
-  //           <div className="py-5">
-  //             <label htmlFor="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Promo Code</label>
-  //             <div className='flex flex-row'>
-  //               <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full" />
-  //               <button className="bg-red-500 hover:bg-red-600 px-3 text-sm text-white uppercase rounded">Apply</button>
-  //             </div>
-  //           </div>
-
-  //           <div className="border-t mt-2">
-  //             <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-  //               <span>Total Amount</span>
-  //               <span>₹{(totalPrice + 10).toFixed(2)}</span>
-  //             </div>
-  //             <button onClick={() => router.push('/checkout1')} className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full rounded">Checkout</button>
-  //           </div>
-  //         </div>
-  //       </div>
 };
 
 export default cart
